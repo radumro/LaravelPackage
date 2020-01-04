4e72deb00a39840277b1f8038077354de5a7ca0b
Radu Martin <radu.martin@radacini-grup.ro>
2020-01-04 06:05:14 +0200

    Added Model file

---
+++ src/app/Models/MyPackage.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class :uc:package extends Model
+{
+    /*
+    public function :uc:package(){
+            return $this->belongsToMany('App\:uc:package');
+    }
+    */
+}
